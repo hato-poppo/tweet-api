f539379632f582a05c5034b99578acc249e940bb
hato-poppo <ysng1224@gmail.com>
2021-05-18 14:29:39 +0000

    [Fix] To class method.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,12 @@
 class Tweet < ApplicationRecord
   belongs_to :user
 
-  def select_all_with_user
-    self.joins(:user).select('tweets.*, users.name AS user_name, users.account_name AS user_account_name').all.order(created_at: :desc)
+  class << self
+
+    def select_all_with_user
+      self.joins(:user).select('tweets.*, users.name AS user_name, users.account_name AS user_account_name').all.order(created_at: :desc)
+    end
+
   end
 
 end
